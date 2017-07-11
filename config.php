ecd94d8bec41687710ce4060ee797ab0d60b7cd2
Rahul Kamboj <kamo.rahul@gmail.com>
2017-07-11 14:52:30 +0530

    config required for docker file
    this is the dependency for the docker file proccess

---
+++ config.php
@@ -0,0 +1,8 @@
+
+<?php
+
+$app['debug'] = false;
+$app['mongodb.client.uri'] = null;
+$app['mongodb.client.uriOptions'] = [];
+$app['mongodb.client.driverOptions'] = [];
+$app['twig.cache_dir'] = sys_get_temp_dir() . '/mongoqp-cache';
